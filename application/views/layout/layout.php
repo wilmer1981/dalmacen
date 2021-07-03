b4b9ebf6d61bd7eadac9602a2cebda3d9d512e7d
unknown <wilmer_1981@hotmail.com>
2021-07-02 21:16:18 -0500

    editado layout

--- application/views/layout/layout.php
+++ application/views/layout/layout.php
@@ -12,6 +12,13 @@ $url       =  $this->uri->segment(1);
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="keywords" content="Zapatillas, Accesorios, Ropas, Laptop, PC, Smartphone, Samsung, LG, SonyErricsson, Motorola" />	
 	<meta name="google-site-verification" content="aqFSrGw-x-e2E1NbVpDF81wx7Vvwzr0N3KBqFTdOquY" />
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KK6SKFG');</script>
+<!-- End Google Tag Manager -->
 	
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
@@ -37,6 +44,10 @@ $url       =  $this->uri->segment(1);
     </script>
  </head>
 <body>
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KK6SKFG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <?php if(isset($header)){ $this->load->view($header); } ?>
 <div class="banner">
